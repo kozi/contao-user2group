8d90d86db0448dcaa0b267711d00496383497569
kozi <martin@kozianka.de>
2014-11-23 12:54:52 +0100

    Changes for packagist/composer

--- contao/languages/en/default.php
+++ contao/languages/en/default.php
@@ -2,11 +2,11 @@
 
 /**
  * Contao Open Source CMS
-* Copyright (C) 2005-2013 Leo Feyer
+* Copyright (C) 2005-2014 Leo Feyer
 *
 *
 * PHP version 5
-* @copyright  Martin Kozianka 2011-2013 <http://kozianka.de/>
+* @copyright  Martin Kozianka 2011-2014 <http://kozianka.de/>
 * @author     Martin Kozianka <http://kozianka.de/>
 * @package    user2group_viewer
 * @license    LGPL
---
+++ contao/modules/ModuleUser2Group.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2014 Leo Feyer
+ *
+ *
+ * PHP version 5
+ * @copyright  Martin Kozianka 2011-2014 <http://kozianka.de/>
+ * @author     Martin Kozianka <http://kozianka.de/>
+ * @package    user2group_viewer
+ * @license    LGPL
+ * @filesource
+ */
+
+/**
+ * Class ModuleUser2Group
+ *
+ * @copyright  Martin Kozianka 2011-2014
+ * @author     Martin Kozianka <http://kozianka.de>
+ * @package    user2group_viewer
+ */
+class ModuleUser2Group extends BackendModule {
+    protected $strTemplate    = 'be_user2group';
+    private $arrGroupList     = array();
+    private $arrUserList      = array();
+
+    protected function compile() {
+        $this->loadLanguageFile('tl_user');
+
+        $GLOBALS['TL_CSS'][] = 'system/modules/user2group_viewer/assets/user2group.css';
+
+        $label['group']      = $GLOBALS['TL_LANG']['tl_user']['groups'][0];
+        $label['user']       = $GLOBALS['TL_LANG']['tl_user']['name'][0];
+        $label['admin']      = $GLOBALS['TL_LANG']['tl_user']['admin_legend'];
+
+        $label['title']      = $GLOBALS['TL_LANG']['MSC']['user2group_title'];
+        $label['no_groups']  = $GLOBALS['TL_LANG']['MSC']['user2group_no_groups'];
+
+
+        $arrGroupList = array();
+        $arrUserList  = array();
+
+
+        // Get groups
+        $objGroupCollection = \UserGroupModel::findAll(array('order' => 'name ASC'));
+        if ($objGroupCollection !== null) {
+            foreach($objGroupCollection as $objGroup) {
+                $this->addGroup($objGroup);
+            }
+        }
+
+        // User
+        $objUserCollection = \UserModel::findAll(array('order' => 'name ASC'));
+        if ($objUserCollection !== null) {
+            foreach($objUserCollection as $objUser) {
+                $this->addUser($objUser);
+            }
+        }
+
+        $this->Template->label     = $label;
+        $this->Template->userList  = $this->arrUserList;
+        $this->Template->groupList = $this->arrGroupList;
+    }
+
+    private function addGroup($objGroup) {
+        $objG           = (object) $objGroup->row();
+        $objG->userList = array();
+
+        $this->arrGroupList[$objG->id] = $objG;
+    }
+
+    private function addUser($objUser) {
+        $objU            = (object) $objUser->row();
+        $objU->groupList = array();
+        $objU->groups    = deserialize($objU->groups);
+
+        if ($objU->groups) {
+            foreach($objU->groups as $gId) {
+                $objU->groupList[] = $this->arrGroupList[$gId];
+
+                $this->arrGroupList[$gId]->userList[] = $objU;
+            }
+        }
+
+        $this->arrUserList[$objU->id] = $objU;
+    }
+}
