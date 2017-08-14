564645e96034e03755f825a4299e5d3f6486b1f6
Martin Kozianka <kozianka@otris.de>
2017-08-14 12:07:00 +0200

    First try for contao 3.5.x & 4.4.x

--- contao/config/config.php
+++ contao/config/config.php
@@ -2,12 +2,12 @@
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2015 Leo Feyer
+ * Copyright (C) 2005-2017 Leo Feyer
  *
  * PHP version 5
- * @copyright  Martin Kozianka 2011-2015
+ * @copyright  Martin Kozianka 2011-2017
  * @author     Martin Kozianka <http://kozianka.de>
- * @package    user2group_viewer
+ * @package    user2group
  * @license    LGPL
  * @filesource
  */
